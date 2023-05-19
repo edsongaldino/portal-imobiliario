2be1713b1e3b9ed6a99f01d0cf19de15f65a9319
Edson Galdino de Araújo <edson.araujo@tjmt.jus.br>
2023-05-19 12:57:33 -0400

    ajuste pagina detalhes

--- resources/views/portal/detalhes.blade.php
+++ resources/views/portal/detalhes.blade.php
@@ -15,22 +15,20 @@
 			<div class="row">
 				<div class="col-lg-7 col-xl-8">
 					<div class="single_property_title mt30-767">
-						<h2>Luxury Family Home</h2>
-						<p>1421 San Pedro St, Los Angeles, CA 90015</p>
+						<h2>Cittá Splendore</h2>
+						<p>Rua D, 155 - Despraiado, Cuiabá - MT</p>
 					</div>
 				</div>
 				<div class="col-lg-5 col-xl-4">
 					<div class="single_property_social_share mt20">
 						<div class="spss float-left fn-400">
 							<ul class="mb0">
-								<li class="list-inline-item"><a href="#"><span class="flaticon-transfer-1"></span></a></li>
 								<li class="list-inline-item"><a href="#"><span class="flaticon-heart"></span></a></li>
 								<li class="list-inline-item"><a href="#"><span class="flaticon-share"></span></a></li>
-								<li class="list-inline-item"><a href="#"><span class="flaticon-printer"></span></a></li>
 							</ul>
 						</div>
 						<div class="price text-right tal-400">
-							<h2>$13,000<small>/mo</small></h2>
+							<h2>R$587.000<small>,00</small></h2>
 						</div>
 					</div>
 				</div>
@@ -76,9 +74,9 @@
 								<div class="lsd_list">
 									<ul class="mb0">
 										<li class="list-inline-item"><a href="#">Apartment</a></li>
-										<li class="list-inline-item"><a href="#">Beds: 4</a></li>
-										<li class="list-inline-item"><a href="#">Baths: 2</a></li>
-										<li class="list-inline-item"><a href="#">Sq Ft: 5280</a></li>
+										<li class="list-inline-item"><span><i class="fa-solid fa-bed"></i> 4</span></li>
+										<li class="list-inline-item"><span><i class="fa-solid fa-shower"></i> 2</span></li>
+										<li class="list-inline-item"><span><i class="fa-solid fa-ruler-combined"></i> 58m²</span></li>
 									</ul>
 								</div>
 								<h4 class="mb30">Description</h4>
@@ -543,7 +541,7 @@
 						<div class="col-lg-6">
 							<div class="feat_property">
 								<div class="thumb">
-									<img class="img-whp" src="images/property/fp1.jpg" alt="fp1.jpg">
+									<img class="img-whp" src="{{ asset('assets/portal/images/property/fp1.jpg') }}" alt="fp1.jpg">
 									<div class="thmb_cntnt">
 										<ul class="tag mb0">
 											<li class="list-inline-item"><a href="#">For Rent</a></li>
@@ -569,7 +567,7 @@
 									</div>
 									<div class="fp_footer">
 										<ul class="fp_meta float-left mb0">
-											<li class="list-inline-item"><a href="#"><img src="images/property/pposter1.png" alt="pposter1.png"></a></li>
+											<li class="list-inline-item"><a href="#"><img src="{{ asset('assets/portal/images/property/pposter1.png') }}" alt="pposter1.png"></a></li>
 											<li class="list-inline-item"><a href="#">Ali Tufan</a></li>
 										</ul>
 										<div class="fp_pdate float-right">4 years ago</div>
@@ -605,7 +603,7 @@
 									</div>
 									<div class="fp_footer">
 										<ul class="fp_meta float-left mb0">
-											<li class="list-inline-item"><a href="#"><img src="images/property/pposter1.png" alt="pposter1.png"></a></li>
+											<li class="list-inline-item"><a href="#"><img src="{{ asset('assets/portal/images/property/pposter1.png') }}" alt="pposter1.png"></a></li>
 											<li class="list-inline-item"><a href="#">Ali Tufan</a></li>
 										</ul>
 										<div class="fp_pdate float-right">4 years ago</div>
@@ -641,7 +639,7 @@
 									</div>
 									<div class="fp_footer">
 										<ul class="fp_meta float-left mb0">
-											<li class="list-inline-item"><a href="#"><img src="images/property/pposter1.png" alt="pposter1.png"></a></li>
+											<li class="list-inline-item"><a href="#"><img src="{{ asset('assets/portal/images/property/pposter1.png') }}" alt="pposter1.png"></a></li>
 											<li class="list-inline-item"><a href="#">Ali Tufan</a></li>
 										</ul>
 										<div class="fp_pdate float-right">4 years ago</div>
@@ -678,7 +676,7 @@
 									</div>
 									<div class="fp_footer">
 										<ul class="fp_meta float-left mb0">
-											<li class="list-inline-item"><a href="#"><img src="images/property/pposter1.png" alt="pposter1.png"></a></li>
+											<li class="list-inline-item"><a href="#"><img src="{{ asset('assets/portal/images/property/pposter1.png') }}" alt="pposter1.png"></a></li>
 											<li class="list-inline-item"><a href="#">Ali Tufan</a></li>
 										</ul>
 										<div class="fp_pdate float-right">4 years ago</div>
@@ -694,7 +692,7 @@
 							<div class="sl_creator">
 								<h4 class="mb25">Listed By</h4>
 								<div class="media">
-									<img class="mr-3" src="images/team/lc1.png" alt="lc1.png">
+									<img class="mr-3" src="{{ asset('assets/portal/images/team/lc1.png') }}" alt="lc1.png">
 									<div class="media-body">
 								    	<h5 class="mt-0 mb0">Samul Williams</h5>
 								    	<p class="mb0">(123)456-7890</p>
@@ -989,7 +987,7 @@
 							<div class="item">
 								<div class="feat_property home7 agent">
 									<div class="thumb">
-										<img class="img-whp" src="images/property/fp1.jpg" alt="fp1.jpg">
+										<img class="img-whp" src="{{ asset('assets/portal/images/property/fp1.jpg') }}" alt="fp1.jpg">
 										<div class="thmb_cntnt">
 											<ul class="tag mb0">
 												<li class="list-inline-item"><a href="#">For Rent</a></li>
@@ -1004,7 +1002,7 @@
 							<div class="item">
 								<div class="feat_property home7 agent">
 									<div class="thumb">
-										<img class="img-whp" src="images/property/fp2.jpg" alt="fp2.jpg">
+										<img class="img-whp" src="{{ asset('assets/portal/images/property/fp2.jpg') }}" alt="fp2.jpg">
 										<div class="thmb_cntnt">
 											<ul class="tag mb0">
 												<li class="list-inline-item"><a href="#">For Rent</a></li>
@@ -1019,7 +1017,7 @@
 							<div class="item">
 								<div class="feat_property home7 agent">
 									<div class="thumb">
-										<img class="img-whp" src="images/property/fp3.jpg" alt="fp3.jpg">
+										<img class="img-whp" src="{{ asset('assets/portal/images/property/fp3.jpg') }}" alt="fp3.jpg">
 										<div class="thmb_cntnt">
 											<ul class="tag mb0">
 												<li class="list-inline-item"><a href="#">For Rent</a></li>
@@ -1034,7 +1032,7 @@
 							<div class="item">
 								<div class="feat_property home7 agent">
 									<div class="thumb">
-										<img class="img-whp" src="images/property/fp4.jpg" alt="fp4.jpg">
+										<img class="img-whp" src="{{ asset('assets/portal/images/property/fp4.jpg') }}" alt="fp4.jpg">
 										<div class="thmb_cntnt">
 											<ul class="tag mb0">
 												<li class="list-inline-item"><a href="#">For Rent</a></li>
@@ -1049,7 +1047,7 @@
 							<div class="item">
 								<div class="feat_property home7 agent">
 									<div class="thumb">
-										<img class="img-whp" src="images/property/fp5.jpg" alt="fp5.jpg">
+										<img class="img-whp" src="{{ asset('assets/portal/images/property/fp5.jpg') }}" alt="fp5.jpg">
 										<div class="thmb_cntnt">
 											<ul class="tag mb0">
 												<li class="list-inline-item"><a href="#">For Rent</a></li>
@@ -1078,7 +1076,7 @@
 					<div class="sidebar_feature_listing">
 						<h4 class="title">Recently Viewed</h4>
 						<div class="media">
-							<img class="align-self-start mr-3" src="images/blog/fls1.jpg" alt="fls1.jpg">
+							<img class="align-self-start mr-3" src="{{ asset('assets/portal/images/blog/fls1.jpg') }}" alt="fls1.jpg">
 							<div class="media-body">
 						    	<h5 class="mt-0 post_title">Nice Room With View</h5>
 						    	<a href="#">$13,000/<small>/mo</small></a>
@@ -1090,7 +1088,7 @@
 							</div>
 						</div>
 						<div class="media">
-							<img class="align-self-start mr-3" src="images/blog/fls2.jpg" alt="fls2.jpg">
+							<img class="align-self-start mr-3" src="{{ asset('assets/portal/images/blog/fls2.jpg') }}" alt="fls2.jpg">
 							<div class="media-body">
 						    	<h5 class="mt-0 post_title">Villa called Archangel</h5>
 						    	<a href="#">$13,000<small>/mo</small></a>
@@ -1102,7 +1100,7 @@
 							</div>
 						</div>
 						<div class="media">
-							<img class="align-self-start mr-3" src="images/blog/fls3.jpg" alt="fls3.jpg">
+							<img class="align-self-start mr-3" src="{{ asset('assets/portal/images/blog/fls3.jpg') }}" alt="fls3.jpg">
 							<div class="media-body">
 						    	<h5 class="mt-0 post_title">Sunset Studio</h5>
 						    	<a href="#">$13,000<small>/mo</small></a>
