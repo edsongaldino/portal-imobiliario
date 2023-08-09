022e6687b5fda81b15838986e99544f4f35ce112
Edson Galdino de Araújo <edson.araujo@tjmt.jus.br>
2023-08-09 17:48:52 -0400

    Ajuste Atualizar Imóvel

--- app/Http/Controllers/IntegracaoController.php
+++ app/Http/Controllers/IntegracaoController.php
@@ -78,6 +78,64 @@ class IntegracaoController extends Controller
 
             if((New Anuncio())->verificaDuplicidade('id_externo', $imovel->ListingID)){
 
+                $dadosAnuncio = Anuncio::where('id_externo',$imovel->ListingID)->first();
+                $anuncio = Anuncio::find($dadosAnuncio->id);
+                $anuncio->finalidade = (New AnuncioFinalidadeController())->GetFinalidadeByTipo($imovel->Details->PropertyType);
+                $anuncio->tipo_publicacao = $this->GetTipoByPublicationType($imovel->PublicationType);
+                $anuncio->tipo_id = (New AnuncioTipoController())->GetIDTipoByNome($imovel->Details->PropertyType);
+                $anuncio->anunciante_id = $anunciante_id;
+                $anuncio->transacao = $this->GetTransacaoByTransactionType($imovel->TransactionType);
+                $anuncio->id_externo = $imovel->ListingID;
+                $anuncio->titulo = $imovel->Title;
+                $anuncio->descricao = $imovel->Details->Description;
+                $anuncio->valor_venda = Helper::converte_reais_to_mysql($imovel->Details->ListPrice ?? 0.00);
+                $anuncio->valor_locacao = Helper::converte_reais_to_mysql($imovel->Details->RentalPrice ?? 0.00);
+                $anuncio->valor_condominio = Helper::converte_reais_to_mysql(0.00);
+                $anuncio->situacao = 'Liberado';
+
+                $endereco = Endereco::find($dadosAnuncio->endereco_id);
+                $endereco->cidade_id = (New EnderecoController())->getIDCidadeByNome($imovel->Location->City) ?? '5103403';
+                $endereco->cep_endereco = Helper::limpa_campo($imovel->Location->PostalCode ?? '78000000');
+                $endereco->logradouro_endereco = $imovel->Location->Address ?? 'Av. do CPA';
+                $endereco->numero_endereco = $imovel->Location->StreetNumber ?? '100';
+                $endereco->complemento_endereco = 'Complemento';
+                $endereco->bairro_endereco = $imovel->Location->Neighborhood ?? 'Centro';
+                $endereco->save();
+
+                if($anuncio->save()){
+
+                    foreach($imovel->Media as $foto){
+
+                        $fotos = new AnuncioFotos();
+                        $fotos->anuncio_id = $anuncio->id;
+                        $fotos->titulo = $foto->Item->attributes()->caption ?? $imovel->Title;
+                        $fotos->arquivo = $foto->Item;
+
+                        if(isset($foto->Item->attributes()->primary)){
+                            $fotos->destaque = 'S';
+                        }else{
+                            $fotos->destaque = 'N';
+                        }
+
+                        $fotos->save();
+                    }
+
+                    $tipo_log = "Sucesso";
+                    $subtipo_log = "Alteração";
+                    $titulo = "Imóvel atualizado com sucesso";
+                    $descricao_log = "Imóvel atualizado com sucesso";
+
+                }else{
+                    $tipo_log = "Erro";
+                    $subtipo_log = "Alteração";
+                    $titulo = "O Imóvel não foi atualizado totalmente";
+                    $descricao_log = "Informações importantes estão ausentes";
+                }
+
+                (New LogIntegracaoAnuncioController())->gravaLogAnuncio($LogIntegracao->id, $imovel->ListingID, $tipo_log, $subtipo_log, $titulo, $descricao_log);
+
+                $total_alterados++;
+
             }else{
 
                 $endereco = new Endereco();
@@ -90,43 +148,13 @@ class IntegracaoController extends Controller
 
                 $endereco->save();
 
-                switch($imovel->TransactionType){
-                    case 'Sale/Rent':
-                        $transacao = 'Locação/Venda';
-                        break;
-                    case 'Sale':
-                        $transacao = 'Venda';
-                        break;
-                    case 'Rent':
-                        $transacao = 'Locação';
-                        break;
-                    default:
-                        $transacao = 'Locação/Venda';
-                        break;
-                }
-
-                switch($imovel->PublicationType){
-                    case 'STANDARD':
-                        $tipo_publicacao = 'STANDARD';
-                        break;
-                    case 'PREMIUM':
-                        $tipo_publicacao = 'PREMIUM';
-                        break;
-                    case 'SUPER_PREMIUM':
-                        $tipo_publicacao = 'PREMIUM';
-                        break;
-                    default:
-                        $tipo_publicacao = 'STANDARD';
-                        break;
-                }
-
                 $anuncio = new Anuncio();
                 $anuncio->finalidade = (New AnuncioFinalidadeController())->GetFinalidadeByTipo($imovel->Details->PropertyType);
-                $anuncio->tipo_publicacao = $tipo_publicacao;
+                $anuncio->tipo_publicacao = $this->GetTipoByPublicationType($imovel->PublicationType);
                 $anuncio->tipo_id = (New AnuncioTipoController())->GetIDTipoByNome($imovel->Details->PropertyType);
                 $anuncio->anunciante_id = $anunciante_id;
                 $anuncio->endereco_id = $endereco->id;
-                $anuncio->transacao = $transacao;
+                $anuncio->transacao = $this->GetTransacaoByTransactionType($imovel->TransactionType);
                 $anuncio->id_externo = $imovel->ListingID;
                 $anuncio->titulo = $imovel->Title;
                 $anuncio->descricao = $imovel->Details->Description;
@@ -175,4 +203,43 @@ class IntegracaoController extends Controller
         (New LogIntegracaoController())->updateLog($LogIntegracao->id, $total_alertas, $total_incluidos, $total_alterados, $total_removidos);
 
     }
+
+    public function GetTransacaoByTransactionType($TransactionType){
+        switch($TransactionType){
+            case 'Sale/Rent':
+                $transacao = 'Locação/Venda';
+                break;
+            case 'Sale':
+                $transacao = 'Venda';
+                break;
+            case 'Rent':
+                $transacao = 'Locação';
+                break;
+            default:
+                $transacao = 'Locação/Venda';
+                break;
+        }
+        return $transacao;
+    }
+
+    public function GetTransacaoByPublicationType($PublicationType){
+        switch($PublicationType){
+            case 'STANDARD':
+                $tipo_publicacao = 'STANDARD';
+                break;
+            case 'PREMIUM':
+                $tipo_publicacao = 'PREMIUM';
+                break;
+            case 'SUPER_PREMIUM':
+                $tipo_publicacao = 'PREMIUM';
+                break;
+            default:
+                $tipo_publicacao = 'STANDARD';
+                break;
+        }
+        return $tipo_publicacao;
+    }
+
+
+
 }
