3df2f1fe376b36f9927c9a8a519e0161728daa4e
Edson Galdino de Araújo <edson.araujo@tjmt.jus.br>
2023-08-30 18:41:19 -0400

    teste caracteres

--- resources/views/portal/detalhes.blade.php
+++ resources/views/portal/detalhes.blade.php
@@ -448,12 +448,12 @@ function initialize() {
     name: 'style_KINESB'
   };
 
-var image = {{ asset('assets/portal/images/resource/mapmarker.png') }};
+var image = "'"{{ asset('assets/portal/images/resource/mapmarker.png') }}"'";
   var marker = new google.maps.Marker({
         position: Kine,
         map: map,
         animation: google.maps.Animation.DROP,
-        title: {{ $anuncio->endereco->logradouro_endereco }} - {{ $anuncio->endereco->bairro_endereco }}, {{ $anuncio->endereco->cidade->nome_cidade }} - {{ $anuncio->endereco->cidade->estado->uf_estado }},
+        title: {{ "'".$anuncio->endereco->logradouro_endereco.', '.$anuncio->endereco->bairro_endereco.', '.$anuncio->endereco->cidade->nome_cidade.'-'.$anuncio->endereco->cidade->estado->uf_estado."'" }},
         icon: image
   });
 
