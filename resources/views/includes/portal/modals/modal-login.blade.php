b7c7925b6cde38069722b52644334bb3b1803e41
Edson Galdino de Araújo <edson.araujo@tjmt.jus.br>
2023-09-15 18:47:35 -0400

    modal centered

--- resources/views/includes/portal/modals/modal-login.blade.php
+++ resources/views/includes/portal/modals/modal-login.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="sign_up_modal modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
